bc3dd1877d6445c3e365e5ac6f72bbee7c060dcd
Filippo Pizzini <fpippo1999@gmail.com>
2018-03-27 12:56:10 +0200

    Add files via upload

---
+++ src/stiletournament/pagine/home.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <div class="wrapper fadeInDown">
+            <div id="formContent2">
+                <h1>Benvenuto, @nomeutente</h1>
+                <br>
+                <h2>Ecco i tuoi tornei:</h2>
+                <br><br>
+                <table id="table" align="center">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Data Creazione</th>
+                        <th>Numero giocatori</th>
+                        <th>Gioco</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Tombolina 5INB</td>
+                        <td>27/03/18</td>
+                        <td>4</td>
+                        <td>Briscola</td>
+                    </tr>
+                </table>
+                <br>
+                <a href="index.php">Nuovo torneo</a>
+                <br>
+                <a href="index.php">Modifica torneo</a>
+                <br>
+                <a href="index.php">Elimina torneo</a>
+                <br><br>
+            </div>
+        </div>
+        <?php
+        ?>
+    </body>
+</html>
