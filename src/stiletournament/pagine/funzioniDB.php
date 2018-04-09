e03977f76be1fef4c1f6f4fcfc5881107d463ffb
filippo pizzini <FILIPPO.PIZZINI@ITT.INFOTELECOM>
2018-04-09 11:41:37 +0200

    Formattazione

--- src/stiletournament/pagine/funzioniDB.php
+++ src/stiletournament/pagine/funzioniDB.php
@@ -1,32 +1,31 @@
 <?php
 
-    function connetti(){
-        $host = "localhost";
-        $user = "root"; //user di default del DBMS
-        $password = ""; //relativa password (è una stringa vuota)
-        $database = "tournament";
-        
-        $connessione = mysqli_connect($host, $user, $password, $database);
-        
-        if(mysqli_error($connessione)){
-            die("Impossibile connettersi al database");
-        }
-        
-        return $connessione;
+function connetti() {
+    $host = "localhost";
+    $user = "root"; //user di default del DBMS
+    $password = ""; //relativa password (è una stringa vuota)
+    $database = "tournament";
+
+    $connessione = mysqli_connect($host, $user, $password, $database);
+
+    if (mysqli_error($connessione)) {
+        die("Impossibile connettersi al database");
     }
-    
-    function disconnetti($connessione){
-        mysqli_close($connessione);
-    }
-    
-    
-    function visualizzaTornei(){
-        $utente = "";//da sessione?
-        $query = "SELECT t.ID, t.Nome, t.DataCreazione, COUNT(s.ID) AS NumeroGiocatori, t.NomeGioco FROM torneo t, utente u, squadra s WHERE t.IDAdmin = u.ID AND u.Nome = '$utente' AND s.IDTorneo = t.ID GROUP BY t.ID";
-        
-        $conn = connetti();
-        $result = mysqli_query($conn, $query);       
-        disconnetti($conn);
-    }
+
+    return $connessione;
+}
+
+function disconnetti($connessione) {
+    mysqli_close($connessione);
+}
+
+function visualizzaTornei() {
+    $utente = ""; //da sessione?
+    $query = "SELECT t.ID, t.Nome, t.DataCreazione, COUNT(s.ID) AS NumeroGiocatori, t.NomeGioco FROM torneo t, utente u, squadra s WHERE t.IDAdmin = u.ID AND u.Nome = '$utente' AND s.IDTorneo = t.ID GROUP BY t.ID";
+
+    $conn = connetti();
+    $result = mysqli_query($conn, $query);
+    disconnetti($conn);
+}
 ?>
 
