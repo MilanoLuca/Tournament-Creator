04d5011b4a053d04cf1d1b2cd8225f7f62908f4c
Filippo Pizzini <fpippo1999@gmail.com>
2018-04-16 12:20:25 +0200

    creazione pagina nuovo torneo + modifica codice
    Ciao!

--- Progetti/stiletournament/pagine/home.php
+++ Progetti/stiletournament/pagine/home.php
@@ -7,12 +7,16 @@
     <body>
 
         <?php
-        if (isset($_GET["reg"])) {
+        if (isset($_GET["del"])) {
             ?>
             <div class="wrapper fadeInDown">
                 <div id="formContent">
+                    <br><br>
+                    <div class="fadeIn first">
+                        <img src="http://www.antiracketliberaimpresa.it/wp-content/uploads/coppa.png" width="30%">
+                    </div>
                     <h2>Sei sicuro di voler cancellare il torneo?</h2>
-
+                    <br><br>
                     <form action="home.php" method="post">
                         <input type="submit" class="fadeIn second" value="SI">
                         <input type="submit" class="fadeIn second" value="NO">
@@ -21,78 +25,49 @@
             </div>
         <?php } else {
             ?>
-
             <div class="wrapper fadeInDown">
                 <div id="formContent2">
                     <h1>Benvenuto, @nomeutente</h1>
+                    <br>
+                    <h2>Ecco i tuoi tornei:</h2>
+                    <br>
+
+                    <table id="table" align="center">
+                        <tr>
+                            <th>●</th>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Data Creazione</th>
+                            <th>Numero giocatori</th>
+                            <th>Gioco</th>
+                            <th>Info</th>
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="selection"><br></td>
+                            <td>1</td>
+                            <td>Tombolina 5INB</td>
+                            <td>27/03/18</td>
+                            <td>4</td>
+                            <td>Briscola</td>
+                            <td><a href="visualizza.php?id=1">+ info</a></td><!-- pagina visualizza torneo -->
+                        </tr>
+                        <tr>
+                            <td><input type="radio" name="selection"><br></td>
+                            <td>2</td>
+                            <td>Tombolin 5INC</td>
+                            <td>09/04/18</td>
+                            <td>4</td>
+                            <td>Briscola</td>
+                            <td><a href="visualizza.php?id=1">+ info</a></td><!-- pagina visualizza torneo -->
+                        </tr>
+                    </table>
                     <br><br>
-                    <form action="" method="post">
-                        <input id="nuovo" name="nuovo" type="submit" class="fadeIn second" value="Crea nuovo torneo">
-                    </form>
-                    <?php
-                    if (isset($_POST["nuovo"])) {
-                        ?>
-                        <div class="wrapper fadeInDown">
-                            <div id="formContent">
-                                <h2>Creazione Torneo</h2>
-                                <br><br>
-                                <div class="fadeIn first">
-                                    <img src="http://www.antiracketliberaimpresa.it/wp-content/uploads/coppa.png" width="30%">
-                                </div>
-                                <br>
-                                <form action="pagine/home.php" method="post">
-                                    <input type="text" id="nomeT" class="fadeIn second" placeholder="Nome Torneo" required>
-                                    <input type="text" id="dataC" class="fadeIn second" placeholder="Data Creazione" required>
-                                    <input type="text" id="numP" class="fadeIn second" placeholder="Numero Partecipanti" required>
-                                    <input type="text" id="tornG" class="fadeIn second" placeholder="Torneo di" required>
-                                    <input type="submit" class="fadeIn third" value="Crea">
-                                </form>
-
-                                <a href="home.php">Torna alla home</a>
-                                <br><br>
-                            </div>
-                        </div>
-                        <?php }
-                        ?>
-                        <h2>Ecco i tuoi tornei:</h2>
-                        <br>
-
-                        <table id="table" align="center">
-                            <tr>
-                                <th>●</th>
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>Data Creazione</th>
-                                <th>Numero giocatori</th>
-                                <th>Gioco</th>
-                                <th>Info</th>
-                            </tr>
-                            <tr>
-                                <td><input type="radio" name="selection"><br></td>
-                                <td>1</td>
-                                <td>Tombolina 5INB</td>
-                                <td>27/03/18</td>
-                                <td>4</td>
-                                <td>Briscola</td>
-                                <td><a href="visualizza.php?id=1">+ info</a></td><!-- pagina visualizza torneo -->
-                            </tr>
-                            <tr>
-                                <td><input type="radio" name="selection"><br></td>
-                                <td>2</td>
-                                <td>Tombolin 5INC</td>
-                                <td>09/04/18</td>
-                                <td>4</td>
-                                <td>Briscola</td>
-                                <td><a href="visualizza.php?id=1">+ info</a></td><!-- pagina visualizza torneo -->
-                            </tr>
-                        </table>
-                        <br>
-                        <a href="modificaTorneo.php">Modifica torneo</a> | <a href="?reg">Elimina torneo</a>
-                        <br><br>
-                    </div>
+                    <a href="nuovoTorneo.php">Nuovo torneo</a> | <a href="modificaTorneo.php">Modifica torneo</a> | <a href="?del">Elimina torneo</a>
+                    <br><br>
                 </div>
-                <?php
-            }
-            ?>
+            </div>
+            <?php
+        }
+        ?>
     </body>
 </html>
--- Progetti/stiletournament/pagine/nuovoTorneo.php
+++ Progetti/stiletournament/pagine/nuovoTorneo.php
@@ -3,23 +3,23 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <title>Modifica</title>
+        <title>Creazione</title>
     </head>
     <body>
         <div class="wrapper fadeInDown">
             <div id="formContent">
-                <h2>Modifica Torneo</h2>
+                <h2>Nuovo Torneo</h2>
                 <br><br>
                 <div class="fadeIn first">
                     <img src="http://www.antiracketliberaimpresa.it/wp-content/uploads/coppa.png" width="30%">
                 </div>
                 <br>
                 <form action="pagine/home.php" method="post">
-                    <input type="text" id="nomeTm" class="fadeIn second" placeholder="Nome Torneo" required>
-                    <input type="text" id="dataCm" class="fadeIn second" placeholder="Data Creazione: " disabled>
-                    <input type="text" id="numPm" class="fadeIn second" placeholder="Numero Partecipanti" required>
-                    <input type="text" id="tornGm" class="fadeIn second" placeholder="Torneo di" required>
-                    <input type="submit" class="fadeIn third" value="Modifica">
+                    <input type="text" id="nomeT" class="fadeIn second" placeholder="Nome Torneo" required>
+                    <input type="text" id="dataC" class="fadeIn second" placeholder="Data Creazione" required>
+                    <input type="text" id="numP" class="fadeIn second" placeholder="Numero Partecipanti" required>
+                    <input type="text" id="tornG" class="fadeIn second" placeholder="Torneo di" required>
+                    <input type="submit" class="fadeIn third" value="Crea">
                 </form>
 
                 <a href="home.php">Torna alla home</a>
