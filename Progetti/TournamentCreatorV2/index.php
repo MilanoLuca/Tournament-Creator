c78bf8901d86e7d6d71249812c2922818c0874c6
Filippo Pizzini <fpippo1999@gmail.com>
2018-05-07 12:25:25 +0200

    CSS su index e form registrazione

--- Progetti/TournamentCreatorV2/index.php
+++ Progetti/TournamentCreatorV2/index.php
@@ -1,40 +1,58 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
-        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
         <?php
         session_start();
-        ?>
-        <h1>Login</h1>
-        <form name="login" method="POST" action="pagine/login.php"> <!-- from per Inserimento dei dati utente per il login-->
-            <table>
-                <tr>    
-                    <td>Username</td>
-                    <td><input type="text" name="user" placeholder="Enter Username"</td>
-                </tr>
-                <tr>    
-                    <td>Password</td>
-                    <td><input type="password" name="pass" placeholder="Enter Password"</td>
-                </tr>
+        ?><?php
+        if (isset($_GET["reg"])) {
+            ?>
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <h2>Registrazione</h2>
 
-            </table>
-            <input type="submit" name="login" value="LOGIN">
+                    <div class="fadeIn first">
+                        <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                    </div>
 
-        </form>
-        <br>
+                    <form action="pagine/signup.php" method="post">
+                        <input type="text" name="user" class="fadeIn second" placeholder="Enter Username" required>
+                        <input type="password" name="pass" class="fadeIn third" placeholder="Enter Password" required>
+                        <input type="password" name="pass" class="fadeIn third" placeholder="Enter Password" required>
+                        <input type="submit" name="login" class="fadeIn fourth" value="LOGIN">
+                    </form>
 
-        <?php
+                    <a href="index.php">Vai all'accesso</a>
+                    <br><br>
+                </div>
+            </div>
+            <?php
+        } else {
+            ?>
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <h2>Accesso</h2>
+
+                    <div class="fadeIn first">
+                        <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                    </div>
+                    <form name="login" method="POST" action="pagine/login.php">
+                        <input type="text" name="user" class="fadeIn second" placeholder="Enter Username" required>
+                        <input type="password" name="pass" class="fadeIn third" placeholder="Enter Password" required>
+                        <input type="submit" name="login" class="fadeIn fourth" value="LOGIN">
+                    </form>
+
+                    <a href="?reg"> Crea nuovo account</a>
+                    <br><br>
+                </div>
+            </div>
+            <?php
+        }
         if (isset($_SESSION["errore"])) {       //se p settata la variabile di sessione "errore" viene visualizzata un messaggio di errore
-            echo "<div> ".$_SESSION["errore"]."</div>";
+            echo "<div> " . $_SESSION["errore"] . "</div>";
             unset($_SESSION["errore"]);
         }
         ?>
